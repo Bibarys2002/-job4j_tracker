792a92ee17fdf6d59afe21b3f76a999be6fbee88
Bibarys <bibaryssultanbek2002@gmail.com>
2025-01-18 22:52:28 +0500

    6. Метод замены заявки. Tracker.replace [#211748 #495178]

--- job4j_tracker/src/main/java/ru/job4j/tracker/Tracker.java
+++ job4j_tracker/src/main/java/ru/job4j/tracker/Tracker.java
@@ -12,31 +12,47 @@ public class Tracker {
         return item;
     }
 
-    public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
-    }
     public Item[] findByName(String key) {
         Item[] result = new Item[size];
         int newSize = 0;
         for (int index = 0; index < size; index++) {
             Item item = items[index];
             if (key.equals(item.getName())) {
-                result[index] = item;
-                newSize++;
+                result[newSize++] = item;
+
             }
         }
         return Arrays.copyOf(items, newSize);
     }
+
     public Item[] findAll() {
         return Arrays.copyOf(items, size);
+    }
+
+    private int indexOf(int id) {
+        int result = -1;
+        for (int index = 0; index < size; index++) {
+        if (items[index].getId() == id) {
+        result = index;
+        break;
+        }
+        }
+        return result;
+        }
+        public Item findById(int id) {
+            int index = indexOf(id);
+            return index != -1 ? items[index] : null;
+        }
+
+    public boolean replace(int id, Item item) {
+        int index = indexOf(id);
+        if (index != -1) {
+            item.setId(id);
+            items[index] =  item;
+            return true;
+            }
+            return false;
+
     }
 
 }
