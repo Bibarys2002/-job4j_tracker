3c51a5ab1461bbfee72c8a7d7e562fc6c2f06948
Bibarys <bibaryssultanbek2002@gmail.com>
2025-01-22 21:30:22 +0500

    5.0. Приведение типов. Повышение типа и понижение типа. [#181322 #496557]

---
+++ job4j_tracker/src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+public class College {
+    public static void main(String[] args) {
+        Freshman freshman = new Freshman();
+        /* делаем приведение к типу родителя Student. */
+        Student student = freshman;
+        /* делаем приведение к типу родителя Object. */
+        Object object = freshman;
+        /* Такое приведение типов называется повышающим (up casting). */
+
+    }
+}
